9a9cce54a3946d1e37a72a442fb3dde7934cf135
mhmadam <am.mardaniyan2@gmail.com>
2022-06-21 17:55:33 +0430

    define User model in account/models.py

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib import admin
+from .models import User
 
 # Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    fieldsets = (
+        (None, {'fields': ('password','username','email')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'picture')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide', ),
+            'fields': ('username', 'email', 'first_name', 'last_name', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('id', 'email', 'first_name', 'last_name', 'is_staff')
+
+admin.site.register(User, UserAdmin)
--- account/models.py
+++ account/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+class User(AbstractUser):
+    first_name = models.CharField(max_length=150, null=True, blank=False, verbose_name="First Name")
+    last_name = models.CharField(max_length=150, null=True, blank=False, verbose_name="Last Name")
+    username = models.CharField(max_length=200, null=True, default=None, unique=True, verbose_name="Username")
+    email = models.EmailField(null=True, unique=True, verbose_name="Email Address")
+    picture = models.ImageField(null=True, blank=True, verbose_name="Profile Picture")
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'username']
+
+    def __str__(self):
+        return f'{self.email}'
